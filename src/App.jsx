eb1bdbde06a9d7dad24ede8bda021ad2eea68bed
kandalkarrohan <kandalkarrohan2020@gmail.com>
2024-05-17 16:18:46 +0530

    Add useCallBack State

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react'
+import { useState,useCallback, useEffect} from 'react'
 
 import './App.css'
 
@@ -8,16 +8,19 @@ function App() {
   const [numbers, setNumbers] = useState(false);
   const [range, setRange] = useState(8);
   const [characters, setCharacters] = useState(false);
-
   
   
   let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let number = "0123456789";
-  let character = "!@#$%&*():><?";
+  let character = "!@#$%&*:><?";
  
-  let upadatePassword = () => {
+  
+
+
+  let upadatePassword = useCallback(() => {
     setPassword(password = "");
     let demoPassword = "";
+    
     if(range != 0){
       if(numbers == true){
         str += number;
@@ -34,18 +37,15 @@ function App() {
          demoPassword += str.charAt(Random);
          setPassword(password += demoPassword.charAt(i));
       }
-   
+     
     }
 
-  }
+  },[range,numbers,characters,name]);
     
-    
-
-    
-   
-  let copy = (event)=>{
+   let copy = (event)=>{
     if(password != ""){
       window.navigator.clipboard.writeText(password);
+     
       alert("Password Copied Succesfully");
     }
     
@@ -55,7 +55,7 @@ function App() {
   return (
     <div className='App'>
        <div className="Input">
-          <input type="text" placeholder='Password'className='password' onChange={(event)=> {setPassword(event.target.value)}} value={password} disabled/>
+          <input type="text" placeholder='Password'className='password' onChange={(event)=> {setPassword(event.target.value)}} value={password} name='password' disabled/>
           <button type='button' onClick={copy}>Copy</button>
        </div>
 
